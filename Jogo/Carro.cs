7536ffd2f833f0c06e3c7fead5850e1ece5e6cf4
Ninjax Gamer <Ninjax_gamer@hotmail.com>
2017-03-27 16:28:57 -0300

    aaaaaaa

--- Jogo/Carro.cs
+++ Jogo/Carro.cs
@@ -8,13 +8,19 @@ namespace Jogo
 {
     class Carro
     {
-        public string modelo = "", cor = "";
-        public int velocidade = 0, marcha = 0;
+        public string modelo = "", cor = "", motor = "";
+        public int velocidade = 0, marcha = 0, largura = 0, comprimento = 0, altura = 0;
 
+        /// <summary>
+        /// ABC
+        /// </summary>
         public void Exibir()
         {
             Console.WriteLine("Carro: " + modelo + " Cor: " + cor + " Marchas: " + marcha + " Velocidade: " + velocidade);
-
         }
+        public void acelerar() { }
+        public void frear() { }
+        public void buzinar() { }
+        public void acender_farois() { }
     }
 }
